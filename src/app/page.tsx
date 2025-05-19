a2d245eb5cad55572651ddce7b3527d07b88e0de
Nizzy <uanandaraja@gmail.com>
2025-05-19 20:43:40 +0700

    basic front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,90 +13,60 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <h1 className="text-2xl">The starter pack</h1>
+        <div className="flex flex-col gap-2">
+          <p className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] tracking-[-.01em] max-w-prose mx-auto sm:mx-0">
+            I hate setting up new projects. You have an idea.
+          </p>
+          <p className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] tracking-[-.01em] max-w-prose mx-auto sm:mx-0">
+            You ended up spending 3 hours doing auth, AGAIN and again.
+          </p>
+          <p className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] tracking-[-.01em] max-w-prose mx-auto sm:mx-0">
+            I don't want to do that any more.
+          </p>
+          <p className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] tracking-[-.01em] max-w-prose mx-auto sm:mx-0">
+            I use: Nextjs, Hono, Postgres, Drizzle, Better Auth, Bun
+          </p>
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="/sign-in"
-            rel="noopener noreferrer"
+          <Button 
+            variant="default" 
+            size="lg"
+            asChild
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Sign in
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <a href="/sign-in" rel="noopener noreferrer">
+              Try signing in
+            </a>
+          </Button>
+          
+          <Button 
+            variant="outline" 
+            size="lg"
+            asChild
           >
-            Read our docs
-          </a>
+            <a
+              href="https://github.com/uanandaraja/next-starterpack"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Star our repo
+            </a>
+          </Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <Button variant="link" asChild className="p-0 h-auto">
+          <a
+            className="flex items-center gap-2"
+            href="https://x.com/nizzyhussle"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @nizzyhussle
+          </a>
+        </Button>
+        
       </footer>
     </div>
   );
